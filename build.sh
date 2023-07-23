6a9e6640681bb359cdba4e5303c578d96b532316
Mineek <mineek@mineek.nl>
2023-07-23 12:06:01 +0200

    netboot

--- build.sh
+++ build.sh
@@ -1,4 +1,52 @@
 ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/aarch64/alpine-minirootfs-3.17.3-aarch64.tar.gz'
+# --- NETBOOT INITRAMFS ---
+mkdir -pv work/initramfs-netboot
+cd work
+curl -sL "$ROOTFS" | tar -xzC initramfs-netboot
+mount -vo bind /dev initramfs-netboot/dev
+mount -vt sysfs sysfs initramfs-netboot/sys
+mount -vt proc proc initramfs-netboot/proc
+cp /etc/resolv.conf initramfs-netboot/etc
+cat << ! > initramfs-netboot/etc/apk/repositories
+http://dl-cdn.alpinelinux.org/alpine/edge/main
+http://dl-cdn.alpinelinux.org/alpine/edge/community
+http://dl-cdn.alpinelinux.org/alpine/edge/testing
+!
+
+sleep 2
+echo "MineekLinux: Copying qemu-aarch64-static to initramfs-netboot"
+cp -v /usr/bin/qemu-aarch64-static initramfs-netboot/usr/bin
+echo "MineekLinux: Installing Alpine Linux packages"
+cat << ! | chroot initramfs-netboot /usr/bin/env PATH=/usr/bin:/usr/local/bin:/bin:/usr/sbin:/sbin /bin/sh
+apk update
+apk upgrade
+apk add bash alpine-base udev unudhcpd busybox-extras
+rc-update add udev
+rc-update add udev-trigger
+rc-update add udev-settle
+!
+
+sleep 2
+rm -v initramfs-netboot/init
+cp -v ../init/init-netboot initramfs-netboot/init
+chmod +x initramfs-netboot/init
+umount -v initramfs-netboot/dev
+umount -v initramfs-netboot/sys
+umount -v initramfs-netboot/proc
+
+cd initramfs-netboot
+sh -c "find . | cpio  --quiet -o -H newc | gzip -9 > ../initramfs-netboot.cpio.gz"
+cd ..
+rm -rvf initramfs-netboot
+echo "MineekLinux: Done, copying initramfs-netboot.cpio.gz to output directory"
+rm -v ../output/ramdisk-netboot.cpio.gz || true
+cp -v initramfs-netboot.cpio.gz ../output/ramdisk-netboot.cpio.gz
+echo "MineekLinux: Done, cleaning up"
+cd ..
+rm -rvf work
+echo "MineekLinux: Alright, now we're going to build the real rootfs"
+
+# --- REAL ROOTFS ---
 umount -v work/rootfs/{dev,sys,proc} >/dev/null 2>&1
 rm -rf work
 mkdir -pv work/rootfs
@@ -30,9 +78,9 @@ rc-update add udev-settle
 !
 
 sleep 2
-rm -v rootfs/init
-cp -v ../init/init rootfs/init
-chmod +x rootfs/init
+# rm -v rootfs/init
+# cp -v ../init/init rootfs/init
+# chmod +x rootfs/init
 umount -v rootfs/dev
 umount -v rootfs/sys
 umount -v rootfs/proc
@@ -95,13 +143,19 @@ cp -v ../copybins/hx-touchd rootfs/usr/bin
 chmod +x rootfs/usr/bin/hx-touchd
 
 cd rootfs
-sh -c "find . | cpio  --quiet -o -H newc | gzip -9 > ../initramfs.cpio.gz"
+# create a ext4 filesystem image of size 5GB
+dd if=/dev/zero of=../rootfs.img bs=1M count=5120
+mkfs.ext4 ../rootfs.img
 cd ..
-rm -rvf rootfs
-echo "MineekLinux: Done, copying initramfs.cpio.gz to output directory"
-rm -v ../output/ramdisk.cpio.gz || true
-cp -v initramfs.cpio.gz ../output/ramdisk.cpio.gz
+mkdir -pv rootfs-mount
+mount -v rootfs.img rootfs-mount
+cp -av rootfs/* rootfs-mount
+umount -v rootfs-mount
+rm -rvf rootfs-mount
+echo "MineekLinux: Done, copying rootfs.img to output directory"
+rm -v ../output/rootfs.img || true
+cp -v rootfs.img ../output/rootfs.img
 echo "MineekLinux: Done, cleaning up"
 cd ..
 rm -rvf work
-echo "MineekLinux: Alright, we're done here, goodbye!"
+echo "MineekLinux: Done!"
